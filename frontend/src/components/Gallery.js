bceffb55f1bff3a11517870bb9fa87850367ea1b
JoeG21 <jcggomez@live.com>
2021-06-25 04:07:48 -0500

    gallery grid

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
 const Gallery = () => {
-    return(
-        <h1>Gallery</h1>
+    return (
+        <div className='container-gallery'>
+            <span className='gallery'> Gallery </span>
+
+            <div className='grid'>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+                <div className='grid-pic'>
+                    hello
+                </div>
+
+
+            </div>
+
+        </div>
     )
 }
 
