e12845328571a182a43f85d3fae9543da8320b8e
Tneebs <tedneben@gmail.com>
2021-07-08 21:58:39 -0500

    reset code for Ted

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import Logo from '../images/astrascapes-black.png';
+import Garden from '../images/garden.webp';
 
 const Header = () => {
     return (
-        <div className='header-container'>
-            <div className='header-upper-container'>
-                <div className='header-logo-container'>
-                    <a href='/' className='header-logo'>
-                        <img src={Logo} className='logo' alt='astrascapes-logo-blk'/>
-                    </a>
+<div className='header-container'>
 
-            <div className='header-top'>
-                <div className='header-logo'>
-                    <a href='/' alt='AstraScapes, LLC.'> AstraScapes, LLC. </a>
-                </div>
+<div className='header-top'>
+    <div className='header-logo'>
+        <a href='/' alt='AstraScapes, LLC.'>
+            <img src={Logo} className='logo' alt='Logo-blk'/>
+        </a>
+    </div>
 
-                <div className='header-navlinks-container'>
-                    <a href='/services' className='navlinks'> Services </a>
-                    <a href='/gallery' className='navlinks'> Gallery </a>
-                    <a href='/contacts' className='navlinks'> Contacts </a>
-                    <a href='/booking' className='navlinks'> Book Online </a>
-                    <a href='/plans' className='navlinks'> Plans {'&'} Pricing </a>
-                </div>
-            </div>
+    <div className='header-navlinks-container'>
+        <a href='/services' className='navlinks'> Services </a>
+        <a href='/gallery' className='navlinks'> Gallery </a>
+        <a href='/contacts' className='navlinks'> Contacts </a>
+        <a href='/booking' className='navlinks'> Book Online </a>
+        <a href='/plans' className='navlinks'> Plans {'&'} Pricing </a>
+    </div>
+</div>
 
 
 
 
 
-            <div className='header-bottom'>
-                    <div className='header-quote'>
-                        <span className='header-des'>
-                            Beautify Your Outdoor and Indoor Space
-                        </span>
+<div className='header-bottom'>
+    <img src={Garden} className='header-pics' />
 
-                        <div className='header-break'></div>
+    <div className='header-quote'>
+        <span className='header-des'>
+            Beautify Your Outdoor
+            <span> {'&'} </span>
+            Indoor Space
+        </span>
 
-                        <span className='get-quote'> GET A QUOTE {'>'} </span>
-                    </div>
-            </div>
-        </div>
+        <div className='header-break'></div>
+
+        <a href='/contacts' className='get-quote'> GET A QUOTE {'>'} </a>
+    </div>
+</div>
+</div>
     )
 }
 
