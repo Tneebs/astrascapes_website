8adf86e4c0ace258fb1a6798d84e14fda0e42485
JoeG21 <jcggomez@live.com>
2021-07-08 22:14:21 -0500

    booking update

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -7,79 +7,78 @@ import Bronze_PW from '../images/bronze_package_pw.jpg';
 
 const Booking = () => {
     return(
-        <div className='booking-main-container'>
-                <h2 className='booking-container-title'>Our Services</h2>
-                <br/>
-                <br/>
-                <div className='booking-main-x-container'>
-                    <div className='booking-left-y-container'>
-                        <img className='booking-left-img' alt='sight-img' src={Sight} />
-                    </div>
-                    <div className='booking-right-y-container'>
-                        <h2 className='booking-right-title'>Sight Consultation</h2>
-                        <p className='booking-right-text-1'>A walk around to assess what needs are wanting to be met.</p>
-                        <p className='booking-right-text-2'>Read More<br/>15 min<br/>Free Consultation</p>
-                        <button className='booking-button'>Book Now</button>
-                    </div>
+        <div className='booking-container'>
+            <p className='booking-title'>Our Services</p>
+            <div className='booking-cards'>
+                <img src={Sight} className='booking-pics' alt='Yard' />
+                <div className='booking-card-des'>
+                    <span className='booking-card-title'> Sight Consultation </span>
+                    <p>
+                        A walk around to assess what needs are wanting to be met.
+                    </p>
+                    <span> Read More </span>
+                    <p> 15 min </p>
+                    <span> Free Consultation </span>
+                    <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Book Now </span> </a>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className='booking-main-x-container'>
-                    <div className='booking-left-y-container'>
-                        <img className='booking-left-img' alt='bronze-lc-img' src={Bronze_LC} />
-                    </div>
-                    <div className='booking-right-y-container'>
-                        <h2 className='booking-right-title'>Bronze Package Lawn Care</h2>
-                        <p className='booking-right-text-1'>Bronze Package Lawn Care, Cut, Edge, Weedeat, and Blow off/Clean-up.</p>
-                        <p className='booking-right-text-2'>Read More<br/>15 min<br/>$40</p>
-                        <button className='booking-button'>Book Now</button>
-                    </div>
+            </div>
+
+            <div className='booking-cards'>
+                <img src={Bronze_LC} className='booking-pics' alt='Lawnmower' />
+                <div className='booking-card-des'>
+                    <span className='booking-card-title'> Bronze Package Lawn Care </span>
+                    <p>
+                        Bronze Package Lawn Care, Cut, Edge, <br /> Weedeat, and Blow off/Clean-up.
+                    </p>
+                    <span> Read More </span>
+                    <p> 15 min </p>
+                    <span> $40 </span>
+                    <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Book Now </span> </a>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className='booking-main-x-container'>
-                    <div className='booking-left-y-container'>
-                        <img className='booking-left-img' alt='silver-lc-img' src={Silver_LC} />
-                    </div>
-                    <div className='booking-right-y-container'>
-                        <h2 className='booking-right-title'>Silver Package Lawn Care</h2>
-                        <p className='booking-right-text-1'>Weekly Lawn maintenance including Bronze Package perks</p>
-                        <p className='booking-right-text-2'>Read More<br/>30 min<br/>$40</p>
-                        <button className='booking-button'>Book Now</button>
-                    </div>
+            </div>
+
+            <div className='booking-cards'>
+                <img src={Silver_LC} className='booking-pics' alt='Sliver-Handshaking' />
+                <div className='booking-card-des'>
+                    <span className='booking-card-title'> Silver Package Lawn Care </span>
+                    <p>
+                        Weekly Lawn maintenance including Bronze Package perks
+                    </p>
+                    <span> Read More </span>
+                    <p> 30 min </p>
+                    <span> $40 </span>
+                    <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Book Now </span> </a>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className='booking-main-x-container'>
-                    <div className='booking-left-y-container'>
-                        <img className='booking-left-img' alt='gold-lc-img' src={Gold_LC} />
-                    </div>
-                    <div className='booking-right-y-container'>
-                        <h2 className='booking-right-title'>Gold Package Lawn Care</h2>
-                        <p className='booking-right-text-1'>Lawn Maintenance to the extreme; Cut, Edge, Weedeat, Blow off and More</p>
-                        <p className='booking-right-text-2'>Read More<br/>30 min<br/>$160</p>
-                        <button className='booking-button'>Book Now</button>
-                    </div>
+            </div>
+
+            <div className='booking-cards'>
+                <img src={Gold_LC} className='booking-pics' alt='Gold-Handshaking' />
+                <div className='booking-card-des'>
+                    <span className='booking-card-title'> Gold Package Lawn Care </span>
+                    <p>
+                        Lawn Maintenance to the extreme; Cut, Edge, <br /> Weedeat, Blow off and More
+                    </p>
+                    <span> Read More </span>
+                    <p> 30 min </p>
+                    <span> $160 </span>
+                    <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Book Now </span> </a>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className='booking-main-x-container'>
-                    <div className='booking-left-y-container'>
-                        <img className='booking-left-img' alt='bronze-pw-img' src={Bronze_PW} />
-                    </div>
-                    <div className='booking-right-y-container'>
-                        <h2 className='booking-right-title'>Bronze Package Pressure Washing</h2>
-                        <p className='booking-right-text-1'>A gentle cleanse of your Walk ways and Front porch.</p>
-                        <p className='booking-right-text-2'>Read More<br/>1 Hr<br/>$200</p>
-                        <button className='booking-button'>Book Now</button>
-                    </div>
+            </div>
+
+            <div className='booking-cards'>
+                <img src={Bronze_PW} className='booking-pics' alt='Power Washing' />
+                <div className='booking-card-des'>
+                    <span className='booking-card-title'> Bronze Package Pressure Washing </span>
+                    <p>
+                        A gentle cleanse of your Walk ways and Front porch.
+                    </p>
+                    <span> Read More </span>
+                    <p> 1 hr </p>
+                    <span> $200 </span>
+                    <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Book Now </span> </a>
                 </div>
-                <br/>
-                <br/>
+            </div>
+
         </div>
     )
 }
