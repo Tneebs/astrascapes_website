0c2ab07a5bbb812499a54a712338d13355633410
JoeG21 <jcggomez@live.com>
2021-07-09 20:59:15 -0500

    update on book online modal

--- frontend/src/components/BookingModal.js
+++ frontend/src/components/BookingModal.js
@@ -1,11 +1,46 @@
 import React from 'react'
 
-const BookingModal = () => {
+const BookingModal = (props) => {
+    if (!props.open) return null
+
     return (
-        <div className='test'>
-            hello
+        <div className='modal-card'>
+
+            <span className='modal-back-button' onClick={props.handleClose}> {'<'} Back </span>
+
+            <span className='modal-title'> {props.selectedCard.title} </span>
+
+            <p> {props.selectedCard.des} </p>
+
+            <span className='modal-time-price'> {props.selectedCard.time} | {props.selectedCard.price} </span>
+
+            <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Contact Us! </span> </a>
+
+            <img src={props.selectedCard.src} className='book-modal-pic' alt='Modal Picture' />
+
+            <div className='modal-bottom-half'>
+                <span> {props.selectedCard.modalDes} </span>
+
+                <p className='modal-book-online'> Book Online </p>
+
+                <span> {props.selectedCard.title} </span>
+                <span> {props.selectedCard.time} | {props.selectedCard.price} </span>
+
+                <a href='/contacts' className='book-now-button' alt='Contact Page'> <span> Contact Us! </span> </a>
+
+                <div className='modal-breaks'></div>
+
+                <p className='modal-location'> Location {'&'} Contact Details </p>
+                <span>
+                    Phone: (405) 301-7271
+                    <br />
+                    Email: steve.penwell@outlook.com
+                    <br />
+                    Location: Client's Place
+                </span>
+            </div>
         </div>
-    )
+            )
 }
 
-export default BookingModal
+            export default BookingModal
